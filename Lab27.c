a946230c4868912897552a2c0c8b06c0c66e633a
Kaushal-Is-Here <82810925+Kaushal-Is-Here@users.noreply.github.com>
2021-04-19 18:05:27 +0530

    Add files via upload

---
+++ Lab27.c
@@ -0,0 +1,43 @@
+//              27.Write a program in C to append multiple lines at the end of a text file. Assume that the content of the file test.txt is :
+
+
+#include <stdio.h>
+
+int main ()
+{
+  FILE * fptr;
+  int i,n;
+  char str[100];
+  char fname[20];
+  char str1;
+  
+	printf(" Input the file name to be opened : ");
+	scanf("%s",fname);		
+  
+    fptr = fopen(fname, "a"); 
+
+    printf(" Input the number of lines to be written : ");
+    scanf("%d", &n);
+    printf(" The lines are : \n");  
+
+    for(i = 0; i < n+1;i++)
+    {
+    fgets(str, sizeof str, stdin);
+    fputs(str, fptr);
+  }
+  fclose (fptr);
+
+	fptr = fopen (fname, "r");  
+	printf("\n The content of the file %s is  :\n",fname);
+	str1 = fgetc(fptr);
+
+	while (str1 != EOF)
+		{
+			printf ("%c", str1);
+			str1 = fgetc(fptr);
+		}
+    printf("\n\n");
+    fclose (fptr);
+
+  return 0;
+}
